e41ce6a507cd94915c96d5ab9b21a7cdf89190d3
Ricardo Leitão Pedro <ricardoleitaopedro@gmail.com>
2025-11-12 19:10:48 +0100

    removed Dockerfile and unused code

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -18,7 +18,10 @@ const prisma = new PrismaClient();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: [/\.vercel\.app$/, 'http://localhost:5173'] }));
+
 app.use(express.json({ limit: '1mb' }));
 app.use(morgan('dev'));
 
